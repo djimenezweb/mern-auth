7e3e82c8a6b5bdffec27912f0d40cf319070de1e
djimenezweb <djimenezweb@gmail.com>
2024-11-26 10:43:14 +0100

    added port to local host

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -1,5 +1,5 @@
 export const corsOptions = {
-  origin: ['http://localhost', 'https://mern-auth-o1gw.onrender.com'],
+  origin: ['http://localhost:5173', 'https://mern-auth-o1gw.onrender.com'],
   methods: 'GET,POST,PUT,DELETE,OPTIONS',
   credentials: true,
   optionsSuccessStatus: 200,
